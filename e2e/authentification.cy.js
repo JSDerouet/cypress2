d8f6117393f3c374f9b7c22e0f4e914ddc9543eb
Derouet <js.derouet@sfr.fr>
2023-05-03 07:41:16 +0200

    add files

---
+++ e2e/authentification.cy.js
@@ -0,0 +1,7 @@
+it('authentification', () => {
+    cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+    cy.get('.mb-6 > .body-2-bold').click();
+    cy.get('#signin-email').type("i.d@gmail.com");
+    cy.get('#signin-password').type("hbfgsFS54");
+    cy.get('[data-qa="signin-submit-button"]').click();
+  })
---
+++ e2e/inscription.cy.js
@@ -0,0 +1,10 @@
+it('inscription', () => {
+    cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+    cy.get('.mb-6 > .body-2-bold').click();
+    cy.get('#firstName').type("js");
+    cy.get('#lastName').type("derouet");
+    cy.get('#signup-email').type("i.d@gmail.com");
+    cy.get('#signup-password').type("hbfgsFS54");
+    cy.get('._2OVE0h6V').click();
+    cy.get('[data-qa="signup-submit-button"]').click();
+  })
